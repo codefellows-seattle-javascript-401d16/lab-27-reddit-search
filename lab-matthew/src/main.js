4aa3d7904afb184df7ec6bdbb0b1ffa4f882dcda
Matthew Parker <cadburylion@outlook.com>
2017-07-18 15:22:28 -0700

    Got superagent get request working successfully

--- lab-matthew/src/main.js
+++ lab-matthew/src/main.js
@@ -2,161 +2,95 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import superagent from 'superagent'
 
-const API_URL = 'http://pokeapi.co/api/v2'
+const API_URL = 'http://www.reddit.com/r'
 
 // create a form container component every time you create a form
 // a form container is a component that holds the state for a forms inputs
-class PokemonForm extends React.Component {
+class SearchForm extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
     this.state = {
-      pokeName: '',
+      textInput: '',
+      numberInput: '',
     }
-
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handlePokeNameChange = this.handlePokeNameChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTextInputChange = this.handleTextInputChange.bind(this);
+    this.handleNumberInputChange = this.handleNumberInputChange.bind(this);
   }
 
-  handlePokeNameChange(e){
-    this.setState({pokeName: e.target.value})
+  handleTextInputChange(e){
+    // console.log('textChange', this);
+    this.setState({textInput: e.target.value})
+  }
+
+  handleNumberInputChange(e){
+    // console.log('numberChange', this);
+    this.setState({numberInput: e.target.value})
   }
 
   handleSubmit(e){
     e.preventDefault()
-    this.props.pokemonSelect(this.state.pokeName)
-  }
+    console.log('handleSubmit', e);
+    superagent.get(`${API_URL}/${this.state.textInput}.json?limit=${this.state.numberInput}`)
+    .then(res => {
+      console.log('superagent result', res);
+  })
+ }
 
   render(){
     return (
-      // ALL INPUTS SHHOULD HAVE THEIR VALUES BOUND TO A STATE
-      // this is called controlled inputs
-      // we create controlled inputs by binding "value" to a state property
-      // and providing an onChange event handler to the input
       <form onSubmit={this.handleSubmit} >
         <input
           type='text'
-          name='pokemonName'
-          placeholder='poke name'
-          value={this.state.pokeName}
-          onChange={this.handlePokeNameChange}
+          name='redditSearch'
+          placeholder='search reddit'
+          value={this.state.textInput}
+          onChange={this.handleTextInputChange}
           />
-      </form>
+        <input
+          type='number'
+          name='numberInput'
+          min='0'
+          max='100'
+          value={this.state.numberInput}
+          onChange={this.handleNumberInputChange}
+          />
+        <input
+          type='submit'
+          name='submitButton'
+          value='search'
+          onClick={this.handleSubmit}
+          />
+        </form>
     )
   }
+}
+
+class SearchResultList extends React.Component {
+
 }
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      pokemonLookup: {},
-      pokemonSelected: null,
-      pokemonNameError: null,
+      topics: [],
     }
-
-    this.pokemonSelect = this.pokemonSelect.bind(this)
   }
-  // called evertime the state is changed
-
-  componentDidUpdate(){
-    console.log('___STATE___', this.state)
-  }
-
-  // this will get called once right before the app componet
-  // gets added to the dom
-  componentDidMount(){
-    console.log('hello wrold')
-    if(localStorage.pokemonLookup){
-      try {
-        let pokemonLookup = JSON.parse(localStorage.pokemonLookup)
-        this.setState({pokemonLookup})
-      } catch(err) {
-        console.log(err)
-      }
-    } else {
-      superagent.get(`${API_URL}/pokemon/`)
-      .then(res => {
-
-        let pokemonLookup = res.body.results.reduce((lookup, n) => {
-          lookup[n.name] = n.url;
-          return lookup
-        }, {})
-
-        try {
-          localStorage.pokemonLookup = JSON.stringify(pokemonLookup)
-          this.setState({pokemonLookup})
-        } catch (err) {
-          console.error(err)
-        }
-      })
-      .catch(console.error)
-    }
-
-  }
-
-  pokemonSelect(name){
-    console.log('cool beans')
-    if(!this.state.pokemonLookup[name]){
-      // do something on state that enables the
-      // view to show an error that that pokemon does not exist
-      this.setState({
-        pokemonSelected: null,
-        pokemonNameError: name,
-      })
-    } else {
-      // make a request to the pokemon api and do something on
-      // state to store the pokemons details to be desplayed to the user
-      superagent.get(this.state.pokemonLookup[name])
-      .then(res => {
-        console.log('selected pokemon', res.body)
-        this.setState({
-          pokemonSelected: res.body,
-          pokemonNameError: null,
-        })
-      })
-      .catch(console.error)
-    }
-
-  }
-
   render(){
     return (
       <div>
-        <h1> form demo </h1>
+        <h1> Search Reddit </h1>
 
-        <PokemonForm pokemonSelect={this.pokemonSelect} />
+        <SearchForm />
 
-        { this.state.pokemonNameError ?
-          <div>
-            <h2> pokemon {this.state.pokemonNameError} does not exist </h2>
-            <p> make another request ! </p>
-          </div> :
-          <div>
-          { this.state.pokemonSelected ?
-            <div>
-              <h2> selected {this.state.pokemonSelected.name} </h2>
-              <p> booyea </p>
-              <h3> abilities </h3>
-              <ul>
-                {this.state.pokemonSelected.abilities.map((item, i) => {
-                  return (
-                    <li key={i}>
-                      <p> {item.ability.name} </p>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div> :
-            <div>
-              <p> make a request </p>
-            </div>
-          }
-          </div>
-        }
       </div>
     )
   }
+
 }
+
+
 
 // create a place to put the app
 const container = document.createElement('div')
