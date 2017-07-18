c68ab24ce5bc13da24cc3fbdf50d47afbab8ea55
Scott <sdmccoy@gmail.com>
2017-07-18 15:43:57 -0700

    search form is displaying

--- lab27-scott/src/main.js
+++ lab27-scott/src/main.js
@@ -2,21 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import superagent from 'superagent';
 
+class SearchForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      SearchFormBoard: '',
+      SearchFormLimit: 0,
+    }
+  };
+
+  render(){
+    return (
+      <div>
+        <form>
+          <input
+          type='text'
+          name='boardName'
+          placeholder='Board Name'
+          />
+        </form>
+      </div>
+    )
+  }
+};
 //start main APP component
 
 class App extends React.Component{
   constructor(props){
     super(props);
 
+    this.state = {
+      topics: [],
+    }
+    //put this. methods here
   };
 
   render(){
     return (
       <div>
-        Hello world
+        <h1>Reddit Board Search</h1>
+        <SearchForm />
       </div>
     )
   }
 };
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
