f1bc51b7a187ae9dd52f4b92935747be836fc982
Scott <sdmccoy@gmail.com>
2017-07-18 14:37:41 -0700

    mainjs is printing to the DOM

--- lab27-scott/src/main.js
+++ lab27-scott/src/main.js
@@ -5,7 +5,18 @@ import superagent from 'superagent';
 //start main APP component
 
 class App extends React.Component{
-  
-}
+  constructor(props){
+    super(props);
+
+  };
+
+  render(){
+    return (
+      <div>
+        Hello world
+      </div>
+    )
+  }
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
