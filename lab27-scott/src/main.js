ffd599b64ade215cce0714ec9365b1a0381c7733
Scott <sdmccoy@gmail.com>
2017-07-18 14:34:47 -0700

    scripts made

---
+++ lab27-scott/src/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import superagent from 'superagent';
+
+//start main APP component
+
+class App extends React.Component{
+  
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
