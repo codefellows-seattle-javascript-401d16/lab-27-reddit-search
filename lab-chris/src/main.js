b369d786c2416c86478f47d2a3fa186d0b07cbeb
janousek77 <janousek77@gmail.com>
2017-07-18 21:43:06 -0700

    finished the lab?

--- lab-chris/src/main.js
+++ lab-chris/src/main.js
@@ -2,133 +2,117 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import superagent from 'superagent'
 
-const API_URL = 'http://pokeapi.co/api/v2'
+const API_URL = 'https://www.reddit.com/r'
 
 class App extends React.Component {
   constructor(props){
-    super(props);
+    super(props)
+    this.state={
+      topics: [],
+    }
+    this.redditSelect = this.redditSelect.bind(this)
+  }
+
+  redditSelect(board, searchLimit){
+    superagent.get(`${API_URL}/${board}.json?limit=${searchLimit}`)
+    .then(res => {
+      console.log(res.body.data.children)
+      this.setState({ topics: res.body.data.children})
+    })
+  }
+
+  render(){
+    return(
+      <div>
+      <SearchForm redditSelect={this.redditSelect}/>
+      <SearchResultList topics={this.state.topics}/>
+      </div>
+    )
+  }
+}
+
+class SearchForm extends React.Component {
+  constructor(props){
+    super(props)
     this.state = {
-      pokeName: '',
-      pokemonLookup: {},
-      pokemonSelected: null,
-      pokemonNameError: null,
+      board: '',
+      searchLimit: 0,
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.pokemonSelect = this.pokemonSelect.bind(this)
-    this.handlePokeNameChange = this.handlePokeNameChange.bind(this)
+    this.handleBoardChange = this.handleBoardChange.bind(this)
+    this.handleLimitChange = this.handleLimitChange.bind(this)
   }
 
-  handlePokeNameChange(e){
-    this.setState({pokeName: e.target.value})
+  handleBoardChange(e){
+    this.setState({board: e.target.value})
+  }
+
+  handleLimitChange(e){
+    this.setState({searchLimit: e.target.value})
   }
 
   handleSubmit(e){
     e.preventDefault()
-    this.pokemonSelect(this.state.pokeName)
+    this.props.redditSelect(this.state.board, this.state.searchLimit)
   }
 
-  componentDidUpdate(){
-    console.log('___STATE___', this.state)
-    console.log('___props___', this.props)
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit} >
+        <input
+          type='text'
+          name='board'
+          placeholder='Choose a subreddit'
+          value={this.state.board}
+          onChange={this.handleBoardChange}
+          />
+        <input
+          type='number'
+          name='searchLimit'
+          placeholder='Number between 1-100'
+          min='0'
+          max='100'
+          value={this.state.searchLimit}
+          onChange={this.handleLimitChange}
+          />
+        <input
+          type='submit'
+          name='submitButton'
+          value='Submit'
+          />
+      </form>
+    )
   }
+}
 
-  componentDidMount(){
-    console.log('hello wrold')
-    if(localStorage.pokemonLookup){
-      try {
-        let pokemonLookup = JSON.parse(localStorage.pokemonLookup)
-        this.setState({pokemonLookup})
-      } catch(err) {
-        console.log(err)
-      }
-    } else {
-      superagent.get(`${API_URL}/pokemon/`)
-      .then(res => {
-
-        let pokemonLookup = res.body.results.reduce((lookup, n) => {
-          lookup[n.name] = n.url;
-          return lookup
-        }, {})
-
-        try {
-          localStorage.pokemonLookup = JSON.stringify(pokemonLookup)
-          this.setState({pokemonLookup})
-        } catch (err) {
-          console.error(err)
-        }
-      })
-      .catch(console.error)
-    }
-
-  }
-
-  pokemonSelect(name){
-    if(!this.state.pokemonLookup[name]){
-      this.setState({
-        pokemonSelected: null,
-        pokemonNameError: name,
-      })
-    } else {
-      superagent.get(this.state.pokemonLookup[name])
-      .then(res => {
-        console.log('selected pokemon', res.body)
-        this.setState({
-          pokemonSelected: res.body,
-          pokemonNameError: null,
-        })
-      })
-      .catch(console.error)
-    }
-
+class SearchResultList extends React.Component {
+  constructor(props){
+    super(props)
   }
 
   render(){
     return (
       <div>
-        <h1> form demo </h1>
-
-        <form onSubmit={this.handleSubmit} >
-          <input
-            type='text'
-            name='pokemonName'
-            placeholder='poke name'
-            value={this.state.pokeName}
-            onChange={this.handlePokeNameChange}
-            />
-        </form>
-
-        { this.state.pokemonNameError ?
-          <div>
-            <h2> pokemon {this.state.pokemonNameError} does not exist </h2>
-            <p> make another request ! </p>
-          </div> :
-          <div>
-          { this.state.pokemonSelected ?
-            <div>
-              <h2> selected {this.state.pokemonSelected.name} </h2>
-              <p> booyea </p>
-              <h3> abilities </h3>
-              <ul>
-                {this.state.pokemonSelected.abilities.map((item, i) => {
-                  return (
-                    <li key={i}>
-                      <p> {item.ability.name} </p>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div> :
-            <div>
-              <p> make a request </p>
-            </div>
+        <h1> Search Results </h1>
+        <ul>
+          {this.props.topics.map((item) => {
+            return (
+              <li key={item.data.id}>
+                <a href={item.data.url}>
+                <h1>{item.data.title}</h1>
+                <p>{item.data.ups}</p>
+                </a>
+              </li>
+              )
+            })
           }
-          </div>
-        }
+        </ul>
       </div>
     )
   }
 }
+
 
 const container = document.createElement('div')
 document.body.appendChild(container)
