b830eda49feb64feb9f62feedae34530972b0265
Amanda Koster <amandakoster@gmail.com>
2017-07-18 18:10:27 -0700

    SearchForm and app are woring

--- lab27-amanda/src/main.js
+++ lab27-amanda/src/main.js
@@ -3,85 +3,107 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import superagent from 'superagent'
 
-const API_URL = 'http://www.reddit.com/r'
 
-class SearchForm extends React.Component{
-  constructor(props){
-    super(props);
-      this.state = {
-        topic: '',
-        }
-        this.handelSubmit = this.handelSubmit.bind(this)
+class SearchForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchValue: '',
+			// limit
+		}
+		this.handelSubmit = this.handelSubmit.bind(this)
+		this.handelChange = this.handelChange.bind(this)
+	}
+	handelChange(e) {
+		this.setState({
+			searchValue: e.target.value
+		})
+	}
+	handelSubmit(e) {
+		e.preventDefault()
+		console.log('handlesubmit');
+		this.props.search(this.state.searchValue)
+	}
+	render() {
+		return (
+      <form onSubmit = {
+				this.handelSubmit
+			}>
+      <input type = 'text'
+			value = {
+				this.state.searchValue
+			}
+			onChange = {
+				this.handelChange
+			}
+      />
+      </form>)
+	}
+}
 
-      }
-      componentDidMount(){
-        superagent.get(`{http://reddit.com/r/${this.state.searchFormBoard}.json?limit=${this.state.searchFormLimit}}`)
-        .then(res => {
-          let topicLookup = res.body.results.reduce((lookup, n) => {
-            lookup[n.topic] = n.url;
-            return lookup
-          }, {})
-          try {
-            this.setState({topicLookup})
-          } catch(err) {
-            console.log(err);
-          }
-        })
-        .catch(console)
-      }
-            handelSubmit(e){
-              this.setState({topic: e.target.value})
-            }
-
-            handelSubmit(e){
-              e.preventDefault()
-              this.props.topicSelect(this.state.topic)
-            }
-
-      render(){
-        return(
-          <form onSubmit={this.handelSubmit}>
-          <input
-          topic= 'topic'
-          value= {this.state.topic}
-          onChange= {this.handelChange}
-          />
-          </form>
-        )
-      }
-    }
-
-  class App extends React.Component{
-    constructor(props){
-      super(props)
-        this.state = {
-          topiclookup: [],
-          topicSelected: null,
-          topicError: null,
-        }
-      }
-
-
-
-    render(){
-      return(
-        <div>
-        <h1> Hello </h1>
-        </div>
+class SearchResultList extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return (
+      <div>
+      <h1> List of top 10 topics </h1>
+      <ul> {this.props.topiclookup.map((item, i) => {
+				return (
+          <li key = {i}>
+          <a href = {
+					item.data.url
+				}>
+        {item.data.title}
+        </a>
+        </li>
       )
-    }
-  }
- // topicSelect(topic){
- //   console.log('topicSelect')
- //   if(!this.state.topicLookup[topic])
- //   this.setState({
- //     topicSelected:null,
- //     topicError:null,
- //   })
- // } else {
- //   superagent
- // }
+			})
+		}
+    </ul>
+    </div>)
+	}
+}
+
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			topiclookup: [],
+			topicSelected: null,
+			topicError: null,
+		}
+		this.search = this.search.bind(this)
+	}
+
+	search(topic, limit) {
+		superagent.get(`https://www.reddit.com/r/${topic}.json?limit=10`).then(res => {
+			console.log(res)
+			this.setState({
+				topiclookup: res.body.data.children
+			})
+			console.log(this.state.topiclookup[0].data.title)
+		})
+	}
+
+	render() {
+		return (
+      <div>
+      <h1> Pick a Reddit Topic, any topic </h1>
+      <SearchForm search = {this.search}
+			/>
+      <div className="inputBox">
+      <SearchResultList topiclookup = {
+				this.state.topiclookup
+			}
+			/>
+      </div>
+      </div>
+    )
+	}
+}
 
 const container = document.createElement('div')
 document.body.appendChild(container)
-ReactDom.render(<App />, container)
+ReactDom.render( < App / > , container)
