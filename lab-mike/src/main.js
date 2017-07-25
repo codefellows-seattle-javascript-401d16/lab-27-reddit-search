378fac267b6b4792c6ed556204a6d7d2d898e182
mikeporter <porter.michael.james@gmail.com>
2017-07-24 18:57:01 -0700

    finished assignment

--- lab-mike/src/main.js
+++ lab-mike/src/main.js
@@ -6,103 +6,122 @@ import superagent from 'superagent';
 const API_URL = 'http://www.reddit.com/r';
 
 class SearchForm extends React.Component {
-
-
-
   constructor(props){
     super(props);
     this.state = {
       subredditName: '',
       responsesToReturn: '',
-    }
+      inputError: null,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleSubredditNameChange = this.handleSubredditNameChange.bind(this);
     this.handleResponsesToReturnChange = this.handleResponsesToReturnChange.bind(this);
   }
   handleSubredditNameChange(event) {
-    this.setState({subredditName: event.target.value})
+    this.setState({subredditName: event.target.value});
   }
 
   handleResponsesToReturnChange(event) {
-    this.setState({responsesToReturn: event.target.value})
+    this.setState({responsesToReturn: event.target.value});
   }
 
   handleSubmit(event) {
     event.preventDefault();
     superagent.get(`${API_URL}/${this.state.subredditName}.json?limit=${this.state.responsesToReturn}`)
       .then((res) => {
-        // on success it should pass the results to the application state
-        this.props.functiontocall(this.state.subreddit, this.state.responsesToReturn)
-        console.log(res.body)
+        this.setState({
+          inputError: null,
+        });
+        this.props.redditSearch(res.body.data.children);
       })
       .catch((err) => {
-                // on failure it should add a class to the form called error and turn the form's inputs borders red
-        console.log(err.message)
-      })
+        this.setState({
+          inputError: true,
+        });
+      });
   }
   render() {
     return (
-      <form onSubmit={this.handleSubmit} >
-      <input
-      type='text'
-      name='subredditName'
-      placeholder='Subreddit Name'
-      value={this.state.subredditName}
-      onChange={this.handleSubredditNameChange}
-      />
-      <input
-      type='number'
-      min='0'
-      max='100'
-      name='responsesToReturn'
-      placeholder='Responses To Return'
-      value={this.state.responsesToReturn}
-      onChange={this.handleResponsesToReturnChange}
-      />
-      <input type="submit" />
+      <form onSubmit={this.handleSubmit}  >
+        <input
+          className={ this.state.inputError ? 'error' : ''}
+          type='text'
+          name='subredditName'
+          placeholder='Subreddit Name'
+          value={this.state.subredditName}
+          onChange={this.handleSubredditNameChange}
+        />
+        <input
+          className={ this.state.inputError ? 'error' : ''}
+          type='number'
+          min='0'
+          max='100'
+          name='responsesToReturn'
+          placeholder='Responses To Return'
+          value={this.state.responsesToReturn}
+          onChange={this.handleResponsesToReturnChange}
+        />
+        <input type="submit" />
       </form>
-    )
+    );
   }
 }
 
 class SearchResultList extends React.Component {
-
-  // If there are topics in the application state it should display the unordered list
-  // Each list item in the unordered list should contain the following
-  // an anchor tag with a href to the topic.url
-  // inside the anchor a heading tag with the topic.title
-  // inside the anchor a p tag with the number of topic.ups
-
   constructor(props){
     super(props);
-
   }
   render() {
     return (
       <div>
+        { this.props.topics.length ?
+          <ul>
+            {this.props.topics.map((item, i) => {
+              return (
+                <li key = {i}>
+                  <a href = {item.data.url}>
+                    <h4>{item.data.title}</h4>
+                    <p>{item.data.ups}</p>
+                  </a>
+                </li>
+              );
+            }
+            )}
+          </ul>
+          :
+          <p> Choose a subreddit and return number of posts </p>
+        }
       </div>
-    )
+    );
   }
 }
 
 class App extends React.Component {
-//   should contain all of the application state
-// should contain methods for modifying the application state
-// the state should have a topics array for holding the results of the search
   constructor(props) {
     super(props);
     this.state = {
       topics: [],
-    }
+    };
+    this.redditSearch = this.redditSearch.bind(this);
+  }
+
+  componentDidUpdate() {
+    console.log('__STATE__', this.state);
+  }
+
+  redditSearch(redditResults) {
+    this.setState({
+      topics: redditResults,
+    });
   }
   render() {
     return (
       <div>
         <h1>Reddit Form</h1>
-        <SearchForm />
-        <SearchResultList />
+        <SearchForm redditSearch = {this.redditSearch} />
+        <SearchResultList topics = {this.state.topics} />
       </div>
-    )
+    );
   }
 }
 
